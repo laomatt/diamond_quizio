efce7bb42881253e233efe0c1d3d7f00ba157ddc
Kailash <kaidurai@gmail.com>
2015-11-10 17:07:22 -0800

    moved categories outside of manage loop

--- config/routes.rb
+++ config/routes.rb
@@ -17,10 +17,12 @@ Rails.application.routes.draw do
     end
 
     resources :questions
-    resources :users
-    resources :categories
-    
+    resources :users    
   end
+
+  resources :categories
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
