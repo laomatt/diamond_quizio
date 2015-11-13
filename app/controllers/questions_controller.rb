a87d5e85c0da72093c3fff39c4396827cb1cde5d
Kailash <kaidurai@gmail.com>
2015-11-12 21:00:53 -0800

    added create to controller, need to access category next

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,34 @@
 class QuestionsController < ApplicationController
-  
-  def new
-  	@user = "hi"
-  	@categories = Category.all
-  	@question = Question.new
-  end
+
+	def new
+		@user = "hi"
+		@question = Question.new
+	end
+
+	def create
+		@user = "hi"
+
+		p"*"*80
+		p question_params
+
+		@question = Question.create(question_params)
+		if @question.save
+			flash[:success] = "question submitted!"
+			render action: "index" 
+		else
+			flash[:danger] = "sorry there was an error"
+			render action: "index"
+		end
+	end
+
+
+
+	private 
+
+
+	def question_params
+		params.require(:question).permit(:question, :real_answer,
+			:dummy_answer1, :dummy_answer2, :dummy_answer3, :citation, :category_id)
+	end
 
 end
