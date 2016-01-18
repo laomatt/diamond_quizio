7107cbced223e6403d211e02515d1bfcfb6e77ea
laomatt <laomatt1@gmail.com>
2016-01-17 22:25:47 -0800

    changed the front page to not display the layout so that the facebook button can be more prminent

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!, :find_user, :except => [:front_page]
-  layout 'public'
+  layout 'public', :except => :front_page
 
   def front_page
   end
